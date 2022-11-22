1f9a231fe185e2efec02a7e5892f7a05cc99f94c
Florian Guillou <guilflo@hotmail.fr>
2022-11-22 02:20:20 -0600

    spec ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
+# Un restaurant doit avoir un nom, une adresse et une catégorie.
+# La catégorie du restaurant doit appartenir à cette liste fixe : ["chinese", "italian", "japanese", "french", "belgian"].
+# Quand un restaurant est supprimé, tous ses avis doivent aussi être supprimés.
+
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
+# Un avis doit appartenir à un restaurant.
+# Un avis doit avoir un contenu.
+# Un avis doit avoir une note.
+# La note d’un avis doit être un nombre entre 0 et 5.
+# La note d’un avis doit être un entier. Par exemple, un avis avec une note de 2,5 doit être invalide !
+
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [*(0..5)] }, numericality: { only_integer: true }
 end
